d50519f8c5a71a6c482535e5d47bad1edc37f99b
mzhKU <ma.hed@bluewin.ch>
2020-09-28 20:46:04 +0200

    cleaned tests

---
+++ testat01/src/test/java/matrix_multiplication/MatrixPerformanceTests.java
@@ -0,0 +1,96 @@
+package matrix_multiplication;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static java.lang.Math.abs;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixPerformanceTests {
+    Matrix a, b, c, d, p, q, k;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        k = new Matrix(2, 2, new double[] {1, 2, 3, 4});
+        a = new Matrix(3, 5, new double[] {1, 2, 3, 4, 5, 2, 3, 4, 5, 6, 3, 4, 5, 6, 7});
+        b = new Matrix(5, 2, new double[] {1,2,3,4,5,6,7,8,9,0});
+        c = new Matrix(3, 2, new double[] {95,60,120,80,145,100});
+        d = new Matrix(3, 3, new double[] {1,0,0,0,1,0,0,0,1});
+        p = new Matrix(3, 3, new double[] {1,2,3,4,5,6,7,8,9});
+        q = new Matrix(3, 3, new double[] {31644432,38881944,46119456,71662158,88052265,104442372,111679884,137222586,162765288});
+    }
+
+    @Test
+    void testEquals() {
+        Matrix r1 = new Matrix(3,3);
+        Matrix r2 = new Matrix(3,3);
+        Matrix zeros = new Matrix(5,5,0);
+        Matrix ones = new Matrix(5,5,1);
+
+        assertTrue(a.equals(a));
+        assertTrue(p.equals(new Matrix(3,3, new double[] {1,2,3,4,5,6,7,8,9})));
+        assertFalse(a.equals(b));
+        assertFalse(a.equals(null));
+        assertFalse(r1.equals(r2));
+
+        assertFalse(ones.equals(zeros));
+    }
+
+    @Test
+    void multiply500x400() {
+        Matrix A = new Matrix(500, 6000);
+        Matrix B = new Matrix(6000, 400);
+
+        long initJava = System.currentTimeMillis();
+        Matrix resultJava = A.multiply(B);
+        long finJava  = System.currentTimeMillis();
+
+        long initCpp = System.currentTimeMillis();
+        Matrix resultCpp = A.multiplyCpp(B);
+        long finCpp = System.currentTimeMillis();
+
+        System.out.println("Java: " + (finJava - initJava) + " milliseconds");
+        System.out.println("C++:  " + (finCpp - initCpp)   + " milliseconds");
+
+        assertTrue(resultJava.equals(resultCpp));
+
+        /*
+         * Performance metrics for Debug version DLL
+         * (VM options "-Djava.library.path="D:\00_fhnw\repo_prcpp\vs_projekte\testat01\PRCPP\x64\Debug")
+         * - Java:  6880 milliseconds
+         * - C++:  15986 milliseconds
+         *
+         * Performance metrics for Release version DLL
+         * (VM options "-Djava.library.path="D:\00_fhnw\repo_prcpp\vs_projekte\testat01\PRCPP\x64\Release")
+         * - Java: 6488 seconds
+         * - C++:  5947 seconds
+         */
+    }
+
+    @Test
+    void testM250Power93() {
+        int POW = 20;
+        int SIZE = 2;
+        // Matrix A1 = new Matrix(SIZE, SIZE, new double[]{1, 2, 3, 4});
+        Matrix A1 = new Matrix(SIZE, SIZE);
+
+        long initJava = System.currentTimeMillis();
+        Matrix resultJava = A1.power(POW);
+        long finJava  = System.currentTimeMillis();
+
+        long initCpp = System.currentTimeMillis();
+        Matrix resultCpp = A1.powerCpp(POW);
+        long finCpp = System.currentTimeMillis();
+
+        // System.out.println("Java: " + (finJava - initJava) + " Milliseconds");
+        // System.out.println("C++:  " + (finCpp - initCpp)   + " Milliseconds");
+        // System.out.println();
+
+        Arrays.stream(resultJava.getValues()).forEach(v -> System.out.println(v));
+        Arrays.stream(resultCpp.getValues()).forEach(v -> System.out.println(v));
+
+        assertEquals(resultJava, resultCpp);
+    }
+}
